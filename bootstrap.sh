ec5d0d7124cdfda460e4b55a48f197c00990a2be
ed <dontemailme@dontemailme.com>
2013-07-10 10:37:39 -0700

    move box location to vagrant file. enabling vm specific settings

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo on
+RUNNER_SCRIPT_DIR=$(cd ${0%/*} && pwd)
+VM_NAME=ubuntuchef1164
+COOKBOOK=cookbook
+vagrant init ${VM_NAME}
+vagrant up
+[ -d ${COOKBOOK} ] || mkdir -p ${COOKBOOK}
